1ade57bf0f94bf47ea915654107481084fcabe4a
Aman <amsingh714@gmail.com>
2019-03-29 13:02:31 -0400

    cleaned code

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const server = require("./api/server");
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log(`\n*** Server is running on http://localhost:${port} ***`)
-})
+  console.log(`\n*** Server is running on http://localhost:${port} ***`);
+});
--- routes/projects-router.js
+++ routes/projects-router.js
@@ -116,11 +116,9 @@ router.delete("/:id", (req, res) => {
       if (count > 0) {
         res.status(200).json(count);
       } else {
-        res
-          .status(404)
-          .json({
-            message: "The project with the specified ID does not exist."
-          });
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
       }
     })
     .catch(error => {
