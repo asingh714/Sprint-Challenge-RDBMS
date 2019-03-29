9d06c5d930e33f2a9b547f8042d7472365e3c96e
Aman <amsingh714@gmail.com>
2019-03-29 12:25:47 -0400

    able to POST actions

--- routes/actions-router.js
+++ routes/actions-router.js
@@ -13,6 +13,38 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const action = req.body;
 
+  if (
+    !action.description ||
+    !action.notes ||
+    action.is_completed === undefined ||
+    action.project_id === undefined
+  ) {
+    res.status(400).json({
+      error:
+        "Please provide a description, notes, is_completed and project_id for the action."
+    });
+  } else {
+    db("actions")
+      .insert(action)
+      .then(ids => {
+        const id = ids[0];
+        db("actions")
+          .where({ id })
+          .first()
+          .then(action => {
+            res.status(201).json(action);
+          })
+          .catch(error => {
+            res.status(500).json({
+              error:
+                "There was an error while saving the action to the database."
+            });
+          });
+      });
+  }
+});
 
 module.exports = router;
