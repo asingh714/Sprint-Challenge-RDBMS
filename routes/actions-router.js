157e100f6db90b9b512d37492bf7b271be45639a
Aman <amsingh714@gmail.com>
2019-03-29 12:57:03 -0400

    able to GET action by id

--- routes/actions-router.js
+++ routes/actions-router.js
@@ -47,4 +47,25 @@ router.post("/", (req, res) => {
   }
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("actions")
+    .where({ id })
+    .first()
+    .then(action => {
+      if (action) {
+        res.status(200).json(action);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The action with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The action with the specified ID could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
